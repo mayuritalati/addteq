167e6535ad9ef20ccdd2d5e9a98e36be5db63962
mayuri.jain <mayuri.jain@atcs.com>
2018-09-12 22:10:13 +0530

    get user REST API
    get user REST API

--- src/main/java/com/example/demo/controller/UserUploadController.java
+++ src/main/java/com/example/demo/controller/UserUploadController.java
@@ -5,35 +5,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.Service.FileStorageService;
 import com.example.demo.model.User;
 import com.example.demo.properties.FileStorageProperties;
+import com.example.demo.repository.UserRepository;
 
 @RestController
+@RequestMapping(value="/user")
 public class UserUploadController {
 
 	@Autowired
-	private FileStorageService fileStorageService;
+	private FileStorageProperties fileStorageProperties;
 
-	@Autowired
-	FileStorageProperties fileStorageProperties;
-
+	
 	private Path fileStorageLocation;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
 	public ArrayList<User> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
@@ -53,5 +52,40 @@ public class UserUploadController {
 
 		return list;
 	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String uploadAndSaveFile(@RequestParam("file") MultipartFile file) throws IOException {
+		this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir()).toAbsolutePath().normalize();
+
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		Path targetLocation = this.fileStorageLocation.resolve(fileName);
+		Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+		ArrayList<User> list = new ArrayList<User>();
+		try (Stream<String> stream = Files.lines(targetLocation.toAbsolutePath())) {
+
+			stream.forEach(line -> userRepository.save(new User(line)));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "saved sucessfully";
+	}
+	
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public User getUserByName(@RequestParam("name") String name) throws IOException {
+		System.out.println("name::"+name);
+		User user = (User) userRepository.findByName(name);
+		return user;
+	}
+	
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ArrayList<User> getUserByName() throws IOException {
+		ArrayList<User> list  = (ArrayList<User>) userRepository.findAll();
+		return list;
+	}
+	
+	
+	
 
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,14 +1,30 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
-	
-	
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private Long id;
+    
+    @Column
+	private String name;
+    
+	public User() {
+		super();
+	}
+
+
 	public User(String name) {
 		super();
 		this.name = name;
 	}
 
-	private String name;
 
 	public String getName() {
 		return name;
@@ -17,5 +33,16 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 
 }
